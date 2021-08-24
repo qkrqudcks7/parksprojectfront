21d68c63e424e7d99dde55c7c62e3c59659f1c17
qkrqudcks7 <66015002+qkrqudcks7@users.noreply.github.com>
2021-08-24 23:33:53 +0900

    main 설정

--- src/main.js
+++ src/main.js
@@ -3,13 +3,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from './custom/axios.custom'
+import store from './store'
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.prototype.axios = axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue({
+const app = new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
+export default app
