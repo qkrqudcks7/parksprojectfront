fef054504621d06bb4ab61023a1720c81ad63870
qkrqudcks7 <66015002+qkrqudcks7@users.noreply.github.com>
2021-08-24 23:33:19 +0900

    vuex store 설정

---
+++ src/store/index.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import {setHeader} from '../custom/axios.custom'
+
+Vue.use(Vuex)
+
+const initUser = (store) => {
+  const {ACCESS_TOKEN} = localStorage
+  if (ACCESS_TOKEN) {
+    store.commit('setToken', ACCESS_TOKEN)
+  }
+}
+
+export default new Vuex.Store({
+  plugins: [initUser],
+  state: {
+    authenticated: false,
+    token: null,
+    currentUser: null
+  },
+  getters: {
+    token (state) {
+      return state.token
+    },
+    user (state) {
+      return state.currentUser
+    },
+    authenticated (state) {
+      return state.authenticated
+    }
+  },
+  mutations: {
+    setToken (state, accessToken) {
+      state.token = accessToken
+      localStorage.ACCESS_TOKEN = accessToken
+      setHeader(accessToken)
+    },
+    setUserDetail (state, payload) {
+      state.currentUser = payload
+      state.authenticated = payload !== null
+    }
+  },
+  actions: {
+    setToken ({commit}, payload) {
+      commit('setToken', payload)
+    },
+    setUserDetail ({commit}, payload) {
+      commit('setUserDetail', payload)
+    }
+  }
+})
