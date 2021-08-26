1fb780a816e5c54936743978dfd0c27013a9d522
qkrqudcks7 <66015002+qkrqudcks7@users.noreply.github.com>
2021-08-27 02:00:58 +0900

    header css

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,61 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <app-header />
+    <br>
+    <b-container class="text-center">
+      <b-row align-h="start">
+        <b-col cols="4" class="bg-primary">ㅌㅇ</b-col>
+        <b-col cols="2" class="bg-danger">gd</b-col>
+        <b-col cols="6" class="bg-dark">gd</b-col>
+      </b-row>
+      <b-row>
+        <b-col sm="2" class="bg-primary">gd</b-col>
+        <b-col sm="8" class="bg-danger">gd</b-col>
+        <b-col sm="2" class="bg-dark">gd</b-col>
+      </b-row>
+      <b-row>
+        <b-col class="bg-primary">gd</b-col>
+        <b-col cols="12" md="auto" class="bg-danger">gd</b-col>
+        <b-col col lg="4" class="bg-dark">gd</b-col>
+      </b-row>
+      <b-row>
+        <b-col cols="12" md="8" class="bg-primary">gd</b-col>
+        <b-col cols="6" md="4" class="bg-danger">gd</b-col>
+      </b-row>
+      <b-row>
+        <b-col md="4" class="bg-primary">gd</b-col>
+        <b-col md="4" offset-md="4" class="bg-danger">gd</b-col>
+        <b-col md="6" offset-md="3" class="bg-dark">md="6" offset-md="3"</b-col>
+      </b-row>
+      <b-row class="mb-3">
+        <b-col md="3" class="bg-primary ml-md-auto p-3">dd</b-col>
+        <b-col md="4" class="bg-danger ml-md-auto">gd</b-col>
+        <b-col class="bg-dark">gd</b-col>
+      </b-row>
+      <b-row class="mb-3">
+        <b-col md="3" class="ml-md-auto p-3 bg-info">dd</b-col>
+        <b-col md="3" class="ml-md-auto p-3 bg-danger">gd</b-col>
+      </b-row>
+      <b-row cols="2">
+        <b-col>dd</b-col>
+        <b-col>dd</b-col>
+        <b-col>dd</b-col>
+        <b-col>dd</b-col>
+      </b-row>
+    </b-container>
     <router-view/>
   </div>
 </template>
 
 <script>
+import appHeader from './components/header'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    appHeader
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,8 +1,20 @@
 <template>
-  <div>
-  </div>
+  <b-container fluid class="text-center">
+    <b-row class="border-bottom">
+      <b-col cols="1" class="p-3">홈페이지</b-col>
+      <b-col cols="1" class="p-3">게시판</b-col>
+      <b-col cols="1" class="p-3">게시판</b-col>
+      <b-col cols="1" offset="7" class="p-3">로그인</b-col>
+      <b-col cols="1" class="p-3">홈페이지</b-col>
+    </b-row>
+    <b-nav tabs justified>
+      <b-nav-item active>Active</b-nav-item>
+      <b-nav-item>Link</b-nav-item>
+      <b-nav-item>Link with a long name </b-nav-item>
+      <b-nav-item disabled>Disabled</b-nav-item>
+    </b-nav>
+  </b-container>
 </template>
-
 <script>
 export default {
   name: 'hheader'
