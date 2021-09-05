afe1439d9d0af0d8418987daea40dff52d199012
qkrqudcks7 <66015002+qkrqudcks7@users.noreply.github.com>
2021-09-06 01:20:32 +0900

    내정보 ui 추가

--- src/components/profile.vue
+++ src/components/profile.vue
@@ -1,14 +1,23 @@
 <template>
   <body>
-    <div>
-      <div v-if="this.$store.getters.user">
+  <section>
+    <div class="row">
+      <div class="columns" v-if="this.$store.getters.user">
         <img v-bind:src="this.$store.getters.user.imageUrl" v-bind:alt="this.$store.getters.user.name" />
       </div>
-      <div class="profile-name" v-if="this.$store.getters.user">
-        <h2>{{this.$store.getters.user.name}}</h2>
-        <p class="profile-email">{{this.$store.getters.user.email}}</p>
+      <div class="columns" v-if="this.$store.getters.user">
+        <div class="third">
+          <label>이름</label>
+          <input type="text" :value="this.$store.getters.user.name" disabled>
+        </div>
+        <div class="third">
+          <label>아이디</label>
+          <input type="text" :value="this.$store.getters.user.email" disabled>
+        </div>
       </div>
     </div>
+    <div>ddddd</div>
+  </section>
   </body>
 </template>
 
@@ -19,5 +28,55 @@ export default {
 </script>
 
 <style scoped>
-
+  body {
+    margin: 0px;
+    font-size: 15px;
+    font-weight: 300;
+  }
+  h1,h2,h3,h4,h5,h6 {
+    margin: 0;
+    font-weight: normal;
+  }
+  button {
+    cursor: pointer;
+    outline: none;
+  }
+  input {
+    outline: none;
+  }
+  section {
+    width: 950px;
+    margin: auto;
+  }
+  .row {
+    display: flex;
+    margin-top: 30px;
+  }
+  .row .columns:nth-child(1) {
+    flex: 1;
+    padding: 20px;
+  }
+  .row .columns:nth-child(2) {
+    flex: 3;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 20px;
+  }
+  .third {
+    display: flex;
+    margin-bottom: 20px;
+    justify-content: space-between;
+  }
+  .third label {
+    flex: 1;
+    text-align: center;
+    font-weight: bold;
+  }
+  .third input {
+    flex: 3;
+  }
+  .columns img {
+    width: 100%;
+  }
 </style>
