91f372939be1230989f0a055d090c26e128ef188
qkrqudcks7 <66015002+qkrqudcks7@users.noreply.github.com>
2021-09-25 00:45:12 +0900

    알림 ui 추가

--- src/components/alarm.vue
+++ src/components/alarm.vue
@@ -5,17 +5,13 @@
       <small>스터디를 생성, 수정, 삭제를 하면 알림을 받을 수 있습니다.</small>
       <div class="alarm">
         <div class="one">
-          <div class="name">
-            알림 기록
-          </div>
+          <div class="name"><p>알림 기록</p><b-icon icon="bell-fill" variant="warning"></b-icon>
+            <small>알림을 <span style="color: crimson">클릭</span>하여 해제</small></div>
           <div class="record">
-            <div class="temp">dd</div>
-            <div class="temp">dd</div>
-            <div class="temp">dd</div>
-            <div class="temp">dd</div>
-            <div class="temp">dd</div>
-            <div class="temp">dd</div>
-            <div class="temp">dd</div>
+            <div class="temp" v-for="(i,index) in alarm" :key="index">
+              <div class="state"><b-icon icon="bell-fill" variant="warning"></b-icon> {{studyCheck(i.notificationType)}}</div>
+              <div class="title">제목: {{i.title}}</div>
+            </div>
           </div>
         </div>
         <div class="two">
@@ -28,7 +24,29 @@
 
 <script>
 export default {
-  name: 'alarm'
+  name: 'alarm',
+  data () {
+    return {
+      alarm: []
+    }
+  },
+  methods: {
+    studyCheck (type) {
+      if (type === 'STUDY_CREATED') {
+        return '스터디 생성'
+      } else if (type === 'STUDY_UPDATED') {
+        return '스터디 업데이트'
+      } else if (type === 'STUDY_DELETE') {
+        return '스터디 삭제'
+      }
+    }
+  },
+  mounted () {
+    this.axios.get(`/notification`).then(response => {
+      this.alarm = response.data
+      console.log(this.alarm)
+    })
+  }
 }
 </script>
 
@@ -37,7 +55,7 @@ body {
   color: black;
   margin: 0;
 }
-h1,h2,h3,h4,h5,h6 {
+h1,h2,h3,h4,h5,h6,p {
   margin: 0;
   font-weight: normal;
 }
@@ -69,6 +87,8 @@ small {
   flex: 1;
   margin: 15px;
   display: flex;
+  border: 1px solid black;
+  padding: 10px;
   flex-direction: column;
 }
 .two {
@@ -80,16 +100,35 @@ small {
 }
 .name {
   border: 1px solid black;
-  padding: 20px;
+  padding: 10px;
   border-radius: 8px;
   margin-bottom: 20px;
   text-align: center;
+}
+.name p {
   font-size: 24px;
 }
 .record {
-  border: 1px solid black;
-  border-radius: 8px;
   display: flex;
   flex-direction: column;
+}
+.temp {
+  padding: 10px;
+  border: 1px solid black;
+  border-radius: 8px;
+  margin-bottom: 5px;
+  /*background-color: #66ff33;*/
+  cursor: pointer;
+}
+.temp:hover {
+  border: 1px solid crimson;
+  box-shadow: 0 0 5px crimson;
+}
+.temp .state {
+  font-size: 20px;
+  margin-bottom: 5px;
+}
+.temp .title {
+  font-size: 15px;
 }
 </style>
